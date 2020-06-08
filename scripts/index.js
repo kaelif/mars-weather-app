78e6fbd4d256952518c68fe8e85940acc7452027
kaelif <flanagan.kaeli@gmail.com>
2020-06-07 23:48:59 -0700

    added api

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+const API_KEY = './api-key.txt'
+const API_URL = 'https://api.nasa.gov/planetary/apod?api_key=PxhlioWiRESGocNewW9mUftQQqbPmKWbP8Pksxrf'
+
 const previousWeatherToggle = document.querySelector('.show-previous-weather');
 
 const previousWeather = document.querySelector('.previous-weather')
@@ -5,3 +8,13 @@ const previousWeather = document.querySelector('.previous-weather')
 previousWeatherToggle.addEventListener('click', () => {
     previousWeather.classList.toggle('show-weather')
 })
+
+getWeather()
+
+function getWeather() {
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+}
